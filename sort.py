8aef11dd920815bbea496c034915845f313fbb3e
pengfc899 <2067397914@qq.com>
2024-02-16 12:25:59 +0800

    提交练习代码

---
+++ sort.py
@@ -0,0 +1,49 @@
+#冒泡排序(bubble sort)
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+#选择排序(selection sort)
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+#插入排序(insertion sort)
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+#希尔排序(shell sort)
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr [j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+
+#归并排序(merge sort)
